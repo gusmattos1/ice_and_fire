2b086eeec78950dcc78dfc8a3a5766b9aef19d1d
gusmattos1 <gusmattos1@gmail.com>
2018-05-03 15:07:46 -0400

    assignment done

--- test/factories/dragons.rb
+++ test/factories/dragons.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :dragon do
     name "Trogdor"
+    age 3
+    rider_id 1
   end
 end
--- test/models/rider_test.rb
+++ test/models/rider_test.rb
@@ -5,8 +5,8 @@ class RiderTest < ActiveSupport::TestCase
   def test_that_riders_swing
     # Use your knowledge of factories to make the test pass!
     # this time use create( ... ) instead of build
-    aegon = nil
-    daenerys = nil
+    aegon = build(:rider, name: "Aegon", sword: "Blackfyre")
+    daenerys = build(:rider, name: "Daenerys", sword: "Lightbringer")
 
     # can't change the assert_ statements!
     skip unless aegon && daenerys
